b3fe730d4d4799864f2679aeb340a6a5321b0fc2
devrr0 <rociofarias002@gmail.com>
2025-03-29 23:09:02 -0300

    Add: Implementation of redirection for information page

--- app/js/listado-actividades.js
+++ app/js/listado-actividades.js
@@ -2,5 +2,9 @@ const redirectIndex = () => {
     window.location.href = "../html/index.html";
 }
 
+const redirectInfo = () => {
+    window.location.href = "../html/info-app.html";
+}
+
 let btnBack = document.getElementById("btn-back");
 btnBack.addEventListener("click", redirectIndex);
