e6eb2903ad8ec2fd64c62a61651707c9d689023e
devrr0 <rociofarias002@gmail.com>
2025-04-04 23:36:55 -0300

    Add: Implementation frontend of add activitie page

--- app/js/index.js
+++ app/js/index.js
@@ -7,8 +7,14 @@ const redirectStats = () => {
     window.location.href = "../html/stats.html";
 }
 
+const redirectAdd = () => {
+    window.location.href = "../html/agregar-actividad.html";
+}
+
 let btnList = document.getElementById("btn-list");
 let btnStats = document.getElementById("btn-stats");
+let btnAdd = document.getElementById("btn-add");
 
 btnList.addEventListener("click", redirectList);
 btnStats.addEventListener("click", redirectStats);
+btnAdd.addEventListener("click", redirectAdd);
