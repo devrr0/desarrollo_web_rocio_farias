d827b805ddd03375643f980961a8560d3868fa1c
devrr0 <rociofarias002@gmail.com>
2025-05-28 13:59:16 -0400

    Add: Validation of comments form

--- flask_app/static/js/info-act.js
+++ flask_app/static/js/info-act.js
@@ -1,10 +1,3 @@
-const redirectIndex = () => {
-    window.location.href = "../home";
-}
-
-const redirectList = () => {
-    window.location.href = "../list-activities";
-}
 
 const expandImage = (btn) => {
     let imageBox = document.getElementById("image-box");
@@ -24,10 +17,52 @@ const redirectInfo = () => {
     image.src = "../img/placeholder.png";
 }
 
-let btnBack = document.getElementById("btn-back");
-let btnList = document.getElementById("btn-list");
-let btnInfo = document.getElementById("close-box");
+// Validacion formulario
 
-btnBack.addEventListener("click", redirectIndex);
-btnList.addEventListener("click", redirectList);
+const validateName = (name) => {
+    if(!name) return false;
+    let maxValid = name.trim().length <= 80;
+    let minValid = name.trim().length >= 3;
+    return maxValid && minValid;
+};
+
+const validateComment = (comment) => {
+    if(!comment) return false;
+    let minValid = comment.trim().length >= 5;
+    return minValid;
+};
+
+const validateForm = () => {
+    let myForm = document.forms["myForm"];
+    let nombre = myForm["nombre"];
+    let comentario = myForm["comentario"];
+    let problemMessageElem = document.getElementById("problem-msg");
+
+    let isValid = true;
+    if(!validateName(nombre.value)){
+        nombre.style.borderColor = "red";
+        isValid = false;
+    } else{
+        nombre.style.borderColor = "";
+    }
+    if(!validateComment(comentario.value)){
+        comentario.style.borderColor = "red";
+        isValid = false;
+    } else{
+        comentario.style.borderColor = "";
+    }
+
+    if (!isValid){
+        problemMessageElem.innerText = "Hay problemas con el formulario";
+    }
+    else{
+        problemMessageElem.innerText = "Se ha subido correctamente su comentario";
+        myForm.submit();
+    }
+}
+
+let btnInfo = document.getElementById("close-box");
+let btnForms = document.getElementById("btn-add-com")
+
+btnForms.addEventListener("click", validateForm)
 btnInfo.addEventListener("click", redirectInfo);
